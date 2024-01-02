45e8fbe42548e3e6be2f019960b1d2c20a4f9978
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-02 19:47:06 -0400

    Adds task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Sum a list of floats using annotations"""
+
+
+def sum_list(input_list: float) -> float:
+    """Return sum of list of floats"""
+    return sum(input_list)
