700ceb1d9f845901a6ee31825fc3b8fe6b8d7009
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-02 19:03:09 -0400

    Updates pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Type-annotated fucntion that adds tow floats"""
 
-def add(a:float, b:float) -> float:
+
+def add(a: float, b: float) -> float:
     """Return sum of two floats as float"""
     return a + b
+
