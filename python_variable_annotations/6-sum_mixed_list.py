571c2171476d11d5ad9163e337648579f6ef1e20
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-02 20:05:53 -0400

    Adds taks 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""suma mixed list and returns a float annotation"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float]) -> float:
+    """returns a float"""
+    return sum(mxd_lst)
