26ddf9dfe66a2629320a703f74bd1034a38aeb7f
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-02 20:59:05 -0400

    UPdates task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -11,4 +11,4 @@ from typing import Iterable, Sequence, Tuple, List
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """duck typing an iterable"""
-    return [Tuple(i, len(i)) for i in lst]
+    return [(i, len(i)) for i in lst]
