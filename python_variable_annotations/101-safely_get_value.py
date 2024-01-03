60036b1af0fb0d7982414d5f3ebc36c7614515ce
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-02 21:51:09 -0400

    Update task 101

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 """Augment the following code with
 the correct duck-typed annotations:"""
-from typing import Mapping, Any, Optional, TypeVar
+from typing import Mapping, Any, Optional, TypeVar, Union
 
 
 T = TypeVar('T')
 
-
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Optional[T] = None) -> Optional[T]:
+                    default: Optional[T] = None) -> Union[Any, T]:
     if key in dct:
         return dct[key]
     else:
