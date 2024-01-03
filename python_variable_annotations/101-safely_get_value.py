80ed5d3fb7cec271a5f9c418dc1162d0167015aa
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-02 21:34:26 -0400

    Update pycode

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -3,9 +3,12 @@
 the correct duck-typed annotations:"""
 from typing import Mapping, Any, Optional, TypeVar
 
+
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Optional[T] = None) -> Optional[T]:
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Optional[T] = None) -> Optional[T]:
     if key in dct:
         return dct[key]
     else:
