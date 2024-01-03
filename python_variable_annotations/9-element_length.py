8385650270ac24d1284144bbacbd583a8bdc62e0
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-02 20:56:09 -0400

    Adds task 9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env pythoin3
+"""Quack goe the duck type
+ _
+<(o )___
+   (  ._> /
+    `---'
+      J
+     """
+from typing import Iterable, Sequence, Tuple, List
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """duck typing an iterable"""
+    return [Tuple(i, len(i)) for i in lst]
