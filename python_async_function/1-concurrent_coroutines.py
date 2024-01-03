e4eb72b5d474e1027e731598e0b4dc95ebc7c7fb
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-03 13:52:23 -0400

    Updates task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,18 +2,13 @@
 """Let's execute multiple coroutines at the same time with async"""
 import asyncio
 
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-
-async def wait_n(maxdelay: int, n: int) -> list:
+async def wait_n(n: int, max_delay: int) -> list:
     """ return the list of all the delays (float values)."""
+    tasks = [wait_random(max_delay) for _ in range(n)]
     delay_list = []
-    for i in range(n):
-        delay_list.append(await wait_random(maxdelay))
-        #sort the list using boubble sort algorithm
-    for item in range(len(delay_list)):
-        for item2 in range(len(delay_list)):
-            if delay_list[item] < delay_list[item2]:
-                delay_list[item], delay_list[item2] = delay_list[item2], delay_list[item]
+    for future in asyncio.as_completed(tasks):
+        result = await future
+        delay_list.append(result)
     return delay_list
