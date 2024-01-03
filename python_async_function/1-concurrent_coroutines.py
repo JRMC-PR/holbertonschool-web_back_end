73930856b335f450146afcfba8c5fad9328964a3
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-03 14:06:41 -0400

    Fix pycode

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,7 +2,9 @@
 """Let's execute multiple coroutines at the same time with async"""
 import asyncio
 
+
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> list:
     """ return the list of all the delays (float values)."""
