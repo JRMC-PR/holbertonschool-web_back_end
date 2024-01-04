0e6e091aae67bda914bfd5d17e0c5b9f3d605b42
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-04 15:35:17 -0400

    Pycode fix

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,8 +2,8 @@
 """Measures the runtime of an async comprehension"""
 import asyncio
 import typing
-
 wait_random = __import__('2-measure_runtime').wait_random
+
 
 async def measure_time(n: int, max_delay: int) -> float:
     """Measures the runtime of an async comprehension"""
