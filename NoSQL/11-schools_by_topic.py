f113ea7e3723231d97629a6aef08649324f4c199
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-11 11:32:54 -0400

    Addst ask 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/urs/bin/env python3
+"""Return the list of schools by topic"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Return the list by topic"""
+    return mongo_collection.find({"topics": topic})
