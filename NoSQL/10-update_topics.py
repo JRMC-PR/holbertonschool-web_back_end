e4b31b5c46c082eb534a52bcbea9675041e379bd
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-11 11:28:07 -0400

    Adss task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""changes all topics of a document based on the name"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
