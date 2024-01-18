8ae55676438365d074934fe6d8c3965261516f3c
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-18 19:00:26 -0400

    Adds task 12

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(...employeesList) {
+  const allEmployees = employeesList;
+  return {
+    allEmployees,
+    getNumberOfDepartments(allEmployees) {
+      return Object.keys(allEmployees).length;
+    },
+  };
+}
