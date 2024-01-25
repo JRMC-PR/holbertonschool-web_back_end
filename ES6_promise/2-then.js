b65a8439a7ce889b1c1b8bebd65c524c0cb7f8c3
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-25 07:56:02 -0400

    Adds task 2 and changes to git ignore

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log("Got a response from the API");
+      return { status: 200, body: 'Success' };
+    })
+    .catch(() => {
+      console.error("An error occurred");
+      return new Error();
+    });
+}
+export default handleResponseFromAPI;
