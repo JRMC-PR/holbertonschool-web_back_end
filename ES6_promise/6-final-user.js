90e5c0f1c066aeab616714bd0ec5259ac550b2c6
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-26 10:14:42 -0400

    change order

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,6 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstname, lastname, filename) {
-  return Promise.all([uploadPhoto(filename), signUpUser(firstname, lastname)])
-    .then((value) => ({ status: 'fulfilled', value }), (error) => ({ status: 'error', error }));
+  return Promise.all([signUpUser(firstname, lastname), uploadPhoto(filename)])
+    .then((value) => ({ status: 'fulfilled', value }), (error) => ({ status: 'rejected', error }));
 }
