85e6988c67c5b1099e0011f4e4d85dfbc0912cb4
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-26 11:00:27 -0400

    Update eslint taks 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -8,22 +8,3 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
     })));
 }
-
-// import signUpUser from './4-user-promise';
-// import uploadPhoto from './5-photo-reject';
-
-// export default function handleProfileSignup(firstName, lastName, fileName) {
-//   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-//     .then((results) => results.map((result) => ({
-//       status: result.status,
-//       value: result.status === 'fulfilled' ? result.value : result.reason,
-//     }))).then((results) => console.log(results));
-// }
-
-// import signUpUser from './4-user-promise';
-// import uploadPhoto from './5-photo-reject';
-
-// export default function handleProfileSignup(firstname, lastname, filename) {
-//   return Promise.allSettled([signUpUser(firstname, lastname), uploadPhoto(filename)])
-//     .then((value) => ({ status: 'fulfilled', value }), (error) => ({ status: 'rejected', error }));
-// }
