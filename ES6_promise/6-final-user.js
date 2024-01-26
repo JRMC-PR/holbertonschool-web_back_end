a901b7a7136bbd998b702b0eb8c3d4edcbdcb932
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-26 09:53:04 -0400

    Updates task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,18 @@
-import  signUpUser  from './4-user-promise';
-import  uploadPhoto  from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstname, lastname, filename) {
-  return Promise.all([uploadPhoto(filename), signUpUser(firstname, lastname)])
-    .then((value) => ({ status: 'success', value }), (error) => ({ status: 'error', error }));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
+
+// import  signUpUser  from './4-user-promise';
+// import  uploadPhoto  from './5-photo-reject';
+
+// export default function handleProfileSignup(firstname, lastname, filename) {
+//   return Promise.all([uploadPhoto(filename), signUpUser(firstname, lastname)])
+//     .then((value) => ({ status: 'success', value }), (error) => ({ status: 'error', error }));
+// }
