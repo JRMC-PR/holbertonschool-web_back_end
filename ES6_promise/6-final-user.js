2b2b5cb8a0132002cd9bb26c17ea8f26b11bdd8b
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-26 09:48:20 -0400

    Update task 6 output

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import  signUpUser  from './4-user-promise';
+import  uploadPhoto  from './5-photo-reject';
 
 export default function handleProfileSignup(firstname, lastname, filename) {
   return Promise.all([uploadPhoto(filename), signUpUser(firstname, lastname)])
