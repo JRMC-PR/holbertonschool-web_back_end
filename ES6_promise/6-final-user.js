738aa8917e26594c43e4d27a4b11ee7f5ed547bb
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-01-26 10:19:52 -0400

    Task 6 variant

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstname, lastname, filename) {
-  return Promise.allSettled([signUpUser(firstname, lastname), uploadPhoto(filename)])
-    .then((value) => ({ status: 'fulfilled', value }), (error) => ({ status: 'rejected', error }));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
+
+// import signUpUser from './4-user-promise';
+// import uploadPhoto from './5-photo-reject';
+
+// export default function handleProfileSignup(firstname, lastname, filename) {
+//   return Promise.allSettled([signUpUser(firstname, lastname), uploadPhoto(filename)])
+//     .then((value) => ({ status: 'fulfilled', value }), (error) => ({ status: 'rejected', error }));
+// }
